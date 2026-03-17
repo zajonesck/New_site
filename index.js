d53d7a226669d3b865fa75c8165e993085426408
Zack Jones <zajonesck83@gmail.com>
2026-03-16 23:48:27 -0500

    Remove granim, use full-bleed CSS background image for hero

--- index.js
+++ index.js
@@ -48,24 +48,4 @@ typewriter
   .pauseFor(3000)
   .start();
 
-var granimInstance = new Granim({
-  element: "#canvas-image-blending",
-  direction: "top-bottom",
-  isPausedWhenNotInView: true,
-  image: {
-    source: "assets/pennybacker.jpg",
-    blendingMode: "multiply",
-  },
-  states: {
-    "default-state": {
-      gradients: [
-        ["#29323c", "#485563"],
-        ["#FF6B6B", "#556270"],
-        ["#80d3fe", "#7ea0c4"],
-        ["#f0ab51", "#eceba3"],
-      ],
-      transitionSpeed: 8000,
-    },
-  },
-});
 
